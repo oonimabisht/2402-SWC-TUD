2aae4fd31f85bd5fc91c870a0ca7dada07143c51
Onima Bisht SCW class <oonimabisht@tudelft.nl>
2024-02-08 14:55:53 +0100

    count_lines.py: added some lines in docstrings
    More strings here thta explain why I did this

--- count_lines.py
+++ count_lines.py
@@ -1,6 +1,13 @@
+"""
+Aim: This script counts the number of lines in standard input
+Input: strings from the comment line
+"""
+
 import sys
 
 count =0
 
 for line in sys.stdin:
 	counts+=1
+
+print(count, "lines in standard input")
